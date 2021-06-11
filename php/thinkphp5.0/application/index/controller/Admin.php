49b63052fa90a95c76cf48293c2bce7abd5fad6e
excaliburKongbai <78746805+excaliburKongbai@users.noreply.github.com>
2021-06-11 13:19:44 +0800

    new_update_time

--- php/thinkphp5.0/application/index/controller/Admin.php
+++ php/thinkphp5.0/application/index/controller/Admin.php
@@ -92,18 +92,218 @@ class admin
                 foreach ($res as $key => $value) {
                     dump($value->toArray());
                 }
-                
-                
-                
-
-
-
-            
-        
-        
         
         //读取多条数据
     }
+
+    //查询并且获取值    
+
+    public function getValue(){
+        //获取单个字段(只能一个)
+            $res = user::where("id",5)->value('name');
+            //匹配的数据-----value('要输出的字段');
+        
+        //获取一列值(可以多带一个列,第二列的参数会变成数组下标)
+            $res = user::column("name","id");
+
+        dump($res);
+    }
+
+    //动态查询(用的少)查询匹配的第一条数据
+    public function dong(){
+        //getBy任意数据库字段
+        $res = User::getByPassword("123");
+        dump($res->toArray());
+    }
+
+    //增加操作
+    public function add(){
+        //实例化增加数据
+            // //先实例化模型
+                // $user = new User;
+            // //设置值
+            //     $user->name = "李四";
+            //     $user->Password = "fwkt";
+            //     $user->age = "18";
+            // //提交数据
+            //     dump($user->save());
+
+        //通过data方法增加
+            // $user->data([
+            //     'name'=>'小明',
+            //     'Password'=>'1234560',
+            //     'age'=>14,
+            // ]);
+            //提交数据
+            // dump($user->save());
+        
+        //实例化方法
+            $user = new User([
+                'name'=>'林北2',
+                'Password'=>'1234560',
+                'age'=>28,
+                'sex'=>'1',
+            ]);
+            //提交数据 
+            dump($user->allowField(true)->save());
+                //allowField(true):屏蔽数据库中不存在的字段
+                //allowField(['name','age']):可以限制写入的字段
+
+        //获取自增的id
+        echo $user->id;
+    }
+    
+    //增加多条数据
+    public function adds(){
+        $user = new User;
+
+        $list=[
+            ['name'=>'李老师','Password'=>'245','age'=>'54'],
+            ['name'=>'切嗣','Password'=>'1245','age'=>'44']
+        ];
+        $user->saveAll($list);
+    }
+
+    //删除
+    public function delete(){ 
+        //删除数据
+            //获取数据再删除  
+                // $user =User::get(['id'=>11]);
+                // dump($user->delete());
+
+            //唯一主键删除destroy(可以删除多条数据,例:"1,2,3")
+                // $user= User::destroy(12);
+                // dump($user);
+
+            //删除指定条件
+                // $user=User::destroy(['name'=>'切嗣']);
+            
+            //删除多个条件
+                // $user=User::destroy(['Password'=>'fwkt','age'=>'18']);
+
+            //闭包删除
+                // $user = User::destroy(function($query){
+                //     $query->where('name','like','%林北%');
+                // });
+                
+
+            // echo $user;
+            
+
+            
+    }
+
+    //修改
+    public function update(){
+        //字段修改
+            // $user =User::get(18);
+            // $user->age=18;
+            // $user->save();
+
+        //数组修改
+            // $user = new User;
+            // $res = $user->save([
+            //     'Password'=>'1314520',
+            //     'age'=>26,
+            // ],['id'=>17]);
+
+        //修改数据(表单传值)
+            //模拟表单传值
+                // $_POST['name']='伞兵';
+                // $_POST['Password']='sbyhlby';
+                // $_POST['age']='26';
+                // $_POST['sex']='nan';
+                // $_POST['id']=5;
+
+            //实例化
+                // $user =new User;
+            //过滤输入的字段,并且修改
+                // $user->allowField(['name','Password','age'])
+                // ->save($_POST,['id' => $_POST['id']]);
+
+        //批量更新数据
+            //声明数据
+                $data= [
+                    ['id'=>'13','name'=>'二哈',"Password"=>'1314520','age'=>'8'],
+                    ['id'=>'15','name'=>'咖啡猫',"Password"=>'1314520','age'=>'12'],
+                ];
+            //实例化
+                $user = new User;
+            //更改数据
+                $user->saveAll($data);
+            
+            
+
+    }
+
+    //聚合(统计数据)
+    public function Statistical(){
+        //查询全部数据
+            $tot=User::count();
+            dump($tot);
+
+        //统计条件判断
+            $tot = User::where('id > 3')->count();
+            dump($tot);
+        
+        //统计最大值(最小/平均/求和 都一样)
+            $max = User::max('age');
+            dump($max);
+    }
+
+    //获取器获取性别
+    public function getSex(){
+        // //获取数据
+        //     $user = User::get(['id',15]);
+        // //经过模型获取器判断输出
+        //     dump($user->toArray());
+        // //不经过模型数据
+        //     dump($user->getData());
+
+    }
+
+    //获取用户状态码
+    public function getStatus(){
+        $user = User::get(['id',3]);
+        dump($user->toArray());
+    }
+
+    //修改器
+    public function setPass(){
+        //修改密码经过模型加密
+        $user = new User;
+        $res =$user->save(['Password'=>'1234'],['id'=>2]);
+        dump($res);
+    }
+
+    //自动完成
+        // 添加
+        public function setAdd(){
+            $user = new User;
+            $res  = $user->save(['name'=>'宗','Password'=>'123','age'=>23,'sex'=>1]);
+            dump($res);
+        }
+
+        //修改
+        public function setSave(){
+            $user = new User;
+            $res  = $user->save(['name'=>'233','Password'=>'123','age'=>20,'sex'=>1],['id'=>20]);
+            dump($res);
+        }
+
+    
+    //软删除
+        public function ruan(){
+            //删除数据
+                // $res = User::destroy(['id'=>19]);
+
+            //查询数据
+                // $res = User::get(['id'=>20]);
+            dump($res);
+        }
+    
+
+
 }
 
 
--- php/thinkphp5.0/application/index/model/Admin.php
+++ php/thinkphp5.0/application/index/model/Admin.php
@@ -4,6 +4,9 @@ namespace app\index\model;
 
 use think\Model;
 
+//引入伪删除类
+use traits\model\SoftDelete;
+
 class admin extends Model
 {
     //连接数据库
@@ -31,5 +34,66 @@ class admin extends Model
     ];
     //设置数据表
     protected $table="user";
+
+    //Sex的获取器(get数据库字段名Attr)
+    public function getsexAttr($val){
+        switch ($val) {
+            case '0':
+                return '未定义';
+                break;
+
+            case '1':
+                return '男';
+                break;
+
+            case '2':
+                return '女';
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
+    //获取用户状态码
+    public function getUserStatusAttr($val){
+
+        $stats = [0=>'未激活',1=>'正常',2=>'限制用户'];
+
+        return $stats[$val];
+    }
+
+    //修改器
+    public function setPasswordAttr($val){
+        //加密
+        return md5($val);
+    }
+
+    //自动修改(很少用)
+        //设置自动更新
+            protected $auto = ['time','User_status'];
+            
+        //书写自动完成
+            protected function setTimeAttr(){
+                return time();
+            }
+            protected function setUserStatusAttr(){
+                return 1;
+            }
+    
+   
+    //自动写入时间戳
+        protected $autoWriteTimestamp = true;
+    
+    //如果数据库字段与预设不一样可以设置
+        protected $createTime = 'create_times';
+        protected $updateTime = 'update_times';
+
+    //软删除
+        //引入类
+            use SoftDelete;
+        //重写字段名
+            protected $deleteTime = 'delete_times';
 
 }
